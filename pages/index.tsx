989577ddb7c3eee5febbe9f822a15f1145c7d15e
Facundo-Romano <facuromano121299@gmail.com>
2023-03-16 20:30:42 -0300

    categories section home

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,23 @@ export default function Home() {
         <h2 className={styles.h2}>Hi Mr. Michael,</h2>
         <h1 className={styles.h1}>Welcome Back!</h1>
         <SearchBar />
+        <div className={styles.categoriesContainer}>
+          <div className={styles.categoriesSubContainer}>
+            <h3 className={styles.h3}>Drink Category</h3>
+            <p className={styles.seeAll}>See All</p>
+          </div>
+          <div className={styles.categoriesSubContainer}>
+            <button className={styles.button}>All</button>
+            <button className={`${styles.button} ${styles.selected}`}>
+              <img className={styles.icon} src="/icons/beer.png" alt="beer icon" />
+              Beer
+            </button>
+            <button className={styles.button}>
+              <img className={styles.icon} src="/icons/wine-glass.png" alt="wine icon" />
+              Wine
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
