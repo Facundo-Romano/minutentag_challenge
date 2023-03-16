02c24e6b1db20c8ada49b74b02a25c5683875f67
Facundo-Romano <facuromano121299@gmail.com>
2023-03-16 19:27:11 -0300

    nav mobile

---
+++ src/components/nav/index.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+import styles from './index.module.css';
+
+export default function Nav() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.navMenu}>
+        <div className={styles.linesContainer}>
+          <div className={styles.line1} />
+          <div className={styles.line2} />
+          <div className={styles.line1} />
+        </div>
+      </div>
+      <Image src="/icons/profile-picture.jpg" width={50} height={50} className={styles.profilePicture} alt="profile picture" />
+    </div>
+  );
+};
