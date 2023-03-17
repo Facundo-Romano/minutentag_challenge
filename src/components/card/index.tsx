0e8258d7c2291a320d9294086a5a737a3d935601
Facundo-Romano <facuromano121299@gmail.com>
2023-03-17 19:31:33 -0300

    navbar responsive

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -3,17 +3,24 @@ import styles from './index.module.css';
 import { CardProps } from '@/types';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const Card: NextPage<CardProps> = ({ brand, image, price, idx, id }) => {
     const priceString = price.toString();
     const cents = priceString.slice(priceString.length-2);
     const dollars = priceString.slice(0, priceString.length-2);
 
+    const imageInPng = (image: string) => {
+        const imgArr = image.split(".");
+
+        return imgArr[0] + ".png";
+    };
+
     return (
         <div className={`${styles.container} ${idx%2 === 0 ? styles.even : styles.odd}`}>
             <Link href={`/${id}-${brand.replace(" ", "-")}`} className={styles.brand}>{brand}</Link>
             <Link href={`/${id}-${brand.replace(" ", "-")}`} className={styles.img}>
-                <Image src={image} fill style={{ objectFit: 'contain' }} alt={`${brand} image`} />
+                <Image src={imageInPng(image)} fill style={{ objectFit: 'contain' }} alt={`${brand} image`} />
             </Link>
             <div className={styles.bottom}>
                 <Link href={`/${id}-${brand.replace(" ", "-")}`} className={styles.price}>{`$${dollars}.${cents}`}</Link>
--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -11,7 +11,17 @@ export default function Nav() {
           <div className={styles.line1} />
         </div>
       </div>
-      <Image src="/icons/profile-picture.jpg" width={40} height={40} className={styles.profilePicture} alt="profile picture" />
+      <div className={styles.navDesktopContainer}>
+        <Image src="/favicon.ico" className={styles.logo} width={60} height={60} alt="logo" />
+        <p className={`${styles.navDesktop} ${styles.active}`}>Home</p>
+        <p className={styles.navDesktop}>Products</p>
+        <p className={styles.navDesktop}>Cart</p>
+        <p className={styles.navDesktop}>Settings</p>
+      </div>
+      <div className={styles.subContainer}>
+        <p className={styles.profileLink}>Profile</p>
+        <Image src="/icons/profile-picture.jpg" width={40} height={40} className={styles.profilePicture} alt="profile picture" />
+      </div>
     </div>
   );
 };
